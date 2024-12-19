16bfd694fb7c095761c74da39a75623e803d1a7b
Stanley Eugene <stanleygordon45@gmail.com>
2024-12-19 14:53:41 -0600

    Initial Commit

---
+++ KoloniReservation/KoloniReservationApp.swift
@@ -0,0 +1,17 @@
+//
+//  KoloniReservationApp.swift
+//  KoloniReservation
+//
+//  Created by Koloni on 19/12/24.
+//
+
+import SwiftUI
+
+@main
+struct KoloniReservationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
